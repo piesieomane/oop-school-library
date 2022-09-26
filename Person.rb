ca7629b3d6a4ab44a5fd65d50e22dc83855e6c1d
piesieomane <piesieyeboah@gmail.com>
2022-09-26 14:46:34 +0000

    Fix linter errors

--- Person.rb
+++ Person.rb
@@ -1,24 +1,25 @@
 class Person
-  attr_accessor :name, :age, :id
-  attr_writer :name, :age
+  attr_accessor :name, :age
+  attr_reader :id
 
-  def initialize(name = 'Unknown', age, parent_permission: true)
+  def initialize(id, age, name = 'Unknown', parent_permission: true)
     @name = name
     @age = age
     @id = id
     @parent_permission = parent_permission
   end
 
-  Private
-  def is_of_age?
-    @age >= 18
-  end
-
   def can_use_services?
-    if is_of_age? || @parent_permission
+    if of_age? || @parent_permission
       true
     else
       false
     end
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
   end
 end
