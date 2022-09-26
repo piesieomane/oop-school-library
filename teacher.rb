795e7d02b2116a063e62cb30a10166a14dc93cf3
piesieomane <piesieyeboah@gmail.com>
2022-09-26 15:04:05 +0000

    Create constructor

--- teacher.rb
+++ teacher.rb
@@ -1,1 +1,9 @@
 require_relative 'person'
+
+class Teacher < Person
+    attr_reader :specialization
+
+    def initialize(id, age, specialization, name = 'Unknown', parent_permission: true)
+        super(id, age, name, parent_permission)
+        @specialization = specialization
+    end
