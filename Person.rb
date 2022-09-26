67487f6a128b51e941f3a086bed546bbaf881e26
piesieomane <piesieyeboah@gmail.com>
2022-09-26 14:11:47 +0000

    Setters for name and age variables

--- Person.rb
+++ Person.rb
@@ -1,9 +1,11 @@
 class Person
     attr_accessor :name, :age, :id
+    attr_writer :name, :age
 
-    def initialize(name, age, id)
+    def initialize(name = "Unknown", age, parent_permission: true)
         @name = name
         @age = age
         @id = id
+        @parent_permission = parent_permission
     end
 end
