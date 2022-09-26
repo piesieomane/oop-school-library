aa8c70e3bd6c7395e020592d2154e88ca6fa2902
piesieomane <piesieyeboah@gmail.com>
2022-09-26 14:54:31 +0000

    Add classroom variable to student class

--- student.rb
+++ student.rb
@@ -1,1 +1,7 @@
 require_relative 'person'
+
+class Student < Person
+    def initialize(id ,age, classroom, name = 'Unknown', parent_permission: true)
+        super(id, age, name, parent_permission)
+        @classroom = classroom
+    end
