c5bfadc58e25132389f4bcbabfe813a9d17fd8ae
piesieomane <piesieyeboah@gmail.com>
2022-09-26 14:59:01 +0000

    Add getter method for classroom variable

--- student.rb
+++ student.rb
@@ -1,7 +1,10 @@
 require_relative 'person'
 
 class Student < Person
+    attr_reader :classroom
+
     def initialize(id ,age, classroom, name = 'Unknown', parent_permission: true)
         super(id, age, name, parent_permission)
         @classroom = classroom
     end
+end
