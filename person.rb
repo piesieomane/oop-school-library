f8c668d0f39fbb6fa935829e9115a1e8e4c7b5e7
piesieomane <piesieyeboah@gmail.com>
2022-09-27 17:28:23 +0000

    Remove the id from the initialize in person class and replace with random

--- person.rb
+++ person.rb
@@ -4,11 +4,11 @@ class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(id, age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @name = name
     @age = age
-    @id = id
+    @id = Random.rand(1..1000)
     @parent_permission = parent_permission
   end
 
