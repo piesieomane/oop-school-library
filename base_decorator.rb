ba7ddaf8f5e484c59d3708b77c296ea81f54bd16
piesieomane <piesieyeboah@gmail.com>
2022-09-27 17:43:06 +0000

    The class CapitalizeDecorator should inherit from BaseDecorator, not Nameable class

--- base_decorator.rb
+++ base_decorator.rb
@@ -13,7 +13,7 @@ class BaseDecorator < Nameable
   end
 end
 
-class CapitalizeDecorator < Nameable
+class CapitalizeDecorator < BaseDecorator
   def correct_name
     @nameable.correct_name.capitalize
   end
