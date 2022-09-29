dd835ef67018b0008db357b9922355b12a684cc8
piesieomane <piesieyeboah@gmail.com>
2022-09-29 20:18:27 +0000

    Create list all book function

--- app.rb
+++ app.rb
@@ -11,7 +11,9 @@ class App
         @rentals = []
     end
 def list_all_books
-    
+    @books.each do |book|
+        puts "Title: #{book.title}, Author: #{book.author}"
+    end
 end
 
 def list_all_people
