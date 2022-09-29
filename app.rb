2f222f2d5de28e566a514a80d549e98d03d36a35
piesieomane <piesieyeboah@gmail.com>
2022-09-29 20:32:15 +0000

    create a person function either student or teacher

--- app.rb
+++ app.rb
@@ -20,6 +20,26 @@ def list_all_people
 end
 
 def create_a_person
+    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]:'
+    person_type = gets.chomp
+    if person_type != '1' && person_type != '2'
+        puts 'Invalid option'
+        return
+    end
+    puts 'Age:'
+    age = gets.chomp
+    puts 'Name:'
+    name = gets.chomp
+    if person_type == '1'
+        puts 'Has parent permission? [Y/N]'
+        parent_permission = gets.chomp
+        parent_permission = parent_permission.downcase == 'y'
+        person = Student.new(age, name, parent_permission)
+    else
+        puts 'Specialization:'
+        specialization = gets.chomp
+        person = Teacher.new(age, specialization, name)
+    end
 end
 
 def create_a_book
