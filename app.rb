b0eec5c9b6d5912b8b8292a9cecee75522d7d1e3
piesieomane <piesieyeboah@gmail.com>
2022-09-29 11:43:10 +0000

    Create empty methods for the all necessary methods

--- app.rb
+++ app.rb
@@ -6,4 +6,19 @@ require_relative 'teacher'
 
 
 def list_all_books
-    
+end
+
+def list_all_people
+end
+
+def create_a_person
+end
+
+def create_a_book
+end
+
+def create_a_rental
+end
+
+def list_all_rentals_for_a_person
+end
