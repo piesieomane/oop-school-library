f7f09ef60c558a9e458d0df0846a692bef371386
piesieomane <piesieyeboah@gmail.com>
2022-10-03 12:06:04 +0000

    Initialise app class in the refactor class

--- refactor.rb
+++ refactor.rb
@@ -1,6 +1,9 @@
 require_relative 'app'
-require 'pry'
 # puts app.create_a_book
+class Refactor
+    def initialize
+        @app = App.new
+    end
 def list_of_options
   puts 'Please choose an option by entering a number:'
   puts '1 - List all books'
@@ -18,17 +21,17 @@ end
 def case_options(option)
   case option
   when 1
-    list_all_books
+    @app.list_all_books
   when 2
-    list_all_people
+    @app.list_all_people
   when 3
-    create_a_person
+    @app.create_a_person
   when 4
-    create_a_book
+    @app.create_a_book
   when 5
-    create_a_rental
+    @app.create_a_rental
   when 6
-    list_all_rentals
+    @app.list_all_rentals
   end
 end
 
@@ -40,10 +43,11 @@ def extra_options(option)
     list_of_options
   end
 end
-
-def main
-  app = App.new
-  app.start
 end
 
-main
+# def main
+#   app = App.new
+#   app.start
+# end
+
+# main
